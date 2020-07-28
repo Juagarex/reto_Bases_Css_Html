a4f2bd44327828ce09978762a9c005c9083e429f
Juan Corredor <rexejej@gmail.com>
2020-07-28 13:44:19 -0500

    correciones de botnoes y bordes

--- src/JS/index.js
+++ src/JS/index.js
@@ -1,12 +1,15 @@
 var mySwiper = new Swiper('.swiper-container', {
-      slidesPerView: 3,
-      spaceBetween: 30,
-      loop: true,
+      centeredSlides: false,
+      
+      setWrapperSize:true,
+      loop: true, 
+     
+      
       // loopFillGroupWithBlank: true,
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      },
+      // pagination: {
+      //   el: '.swiper-pagination',
+      //   clickable: true,
+      // },
       navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
