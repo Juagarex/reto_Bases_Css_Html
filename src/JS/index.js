a6b880221f3c71c5a7f866fcf71ffa6d67add20e
Juan Corredor <rexejej@gmail.com>
2020-08-01 13:58:54 -0500

    correcion bug del slider

--- src/JS/index.js
+++ src/JS/index.js
@@ -1,15 +1,13 @@
 var mySwiper = new Swiper('.swiper-container', {
       centeredSlides: false,
-      
+      slidesPerView: 'auto',
+      spaceBetween: 30,
       setWrapperSize:true,
       loop: true, 
-     
-      
-      // loopFillGroupWithBlank: true,
-      // pagination: {
-      //   el: '.swiper-pagination',
-      //   clickable: true,
-      // },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
       navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
